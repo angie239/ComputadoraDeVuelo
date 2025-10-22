bc0381723fcb2981bda080e136010fc01cb7f9e8
AngélicaRosales <angevarr239@gmail.com>
2025-10-22 17:22:59 -0600

    2da actualizacion
    ola

--- Codigo_ComputadoraDeVuelo/Codigo_ComputadoraDeVuelo.ino
+++ Codigo_ComputadoraDeVuelo/Codigo_ComputadoraDeVuelo.ino
@@ -1,3 +1,9 @@
+#include <Wire.h>
+#include <Adafruit_Sensor.h>
+#include <Adafruit_HMC5883_U.h>
+#include <Adafruit_BMP085.h>
+#include <Adafruit_MPU6050.h>
+
 void setup() {
   // put your setup code here, to run once:
 
